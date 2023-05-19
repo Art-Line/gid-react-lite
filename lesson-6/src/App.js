df6afb83a51298d607559e9e548fbf442c230fb0
Art_Line <serg20004@ukr.net>
2023-05-19 21:13:44 +0200

    react class component

---
+++ lesson-6/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Footer from './footer/Footer';
+import Header from './header/Header';
+import Main from './main/Main';
+
+class App extends React.Component {
+	render() {
+		return (
+			<>
+				<Header />
+				<Main />
+				<Footer /></>
+		)
+	}
+}
+
+export default App;
---
+++ lesson-6/src/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+class Footer extends React.Component {
+    render() {
+        return(
+            <footer className={{color: 'red'}}>
+                footer
+            </footer>
+        )
+    }
+}
+
+export default Footer;
